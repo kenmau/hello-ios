0bb9fe6fa5964e7e1e0196969b087819c5c5dc3a
Ken Mau <>
2021-07-15 22:20:47 -0400

    I am Rich project commit.

---
+++ udemy/ios13-bootcamp/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Ken Mau on 2021-07-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
