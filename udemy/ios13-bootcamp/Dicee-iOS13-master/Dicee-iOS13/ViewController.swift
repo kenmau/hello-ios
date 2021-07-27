1823e4f5fa6ebe0a37b9467e1f0b05d58f30bbac
Ken Mau <>
2021-07-27 11:20:24 -0400

    Lesson 37

--- udemy/ios13-bootcamp/Dicee-iOS13-master/Dicee-iOS13/ViewController.swift
+++ udemy/ios13-bootcamp/Dicee-iOS13-master/Dicee-iOS13/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Reference UI element
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Image Literal
+        diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageViewOne.alpha = 0.5
+        
+        diceImageViewTwo.image = UIImage(named: "DiceTwo")
     }
 
 
