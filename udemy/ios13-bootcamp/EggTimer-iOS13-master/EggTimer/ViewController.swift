3705da793ab24286383de45e7f1f50a7584edeea
Ken Mau <ken_mau@hotmail.com>
2021-07-30 08:52:30 -0400

    Lesson 88

--- udemy/ios13-bootcamp/EggTimer-iOS13-master/EggTimer/ViewController.swift
+++ udemy/ios13-bootcamp/EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -10,14 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    var totalTime: Int = 0
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle ?? "Default"
+        let hardness = sender.currentTitle!
         
-        let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12, "Default" : 0]
+        let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720, "Default" : 0]
         print(eggTimes[hardness]!)
         
 //        switch hardness {
@@ -30,5 +28,16 @@ class ViewController: UIViewController {
 //        default:
 //            print("Unknown Hardness")
 //        }
+        
+        totalTime = eggTimes[hardness] ?? 0
+        
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { timer in
+            if self.totalTime >= 0 {
+                print("\(self.totalTime) seconds.")
+            } else {
+                timer.invalidate()
+            }
+            self.totalTime -= 1
+        })
     }
 }
--- udemy/ios13-bootcamp/MyPlayground.playground/Contents.swift
+++ udemy/ios13-bootcamp/MyPlayground.playground/Contents.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 var password: String = ""
@@ -21,3 +23,13 @@ func greeting2(whoToGreet: String) {
 }
 
 greeting2(whoToGreet: "Angela")
+
+var player1Username: String? = nil
+player1Username = "jackbauerisawesome"
+var unwrappedP1Username = player1Username!
+player1Username = nil
+
+if player1Username != nil {
+    print(player1Username!)
+}
+
