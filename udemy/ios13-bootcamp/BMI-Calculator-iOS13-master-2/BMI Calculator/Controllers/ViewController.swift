449885731551c11a9cdcabc290bd2e8a68cd7f40
Ken Mau <>
2021-11-11 13:14:16 -0500

    BMI-calculator-2

---
+++ udemy/ios13-bootcamp/BMI-Calculator-iOS13-master-2/BMI Calculator/Controllers/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Angela Yu on 21/08/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        print("Height Value: \(sender.value)")
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
+    }
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        print("Weight Value: \(sender.value)")
+        let weight = String(format: "%.0f", sender.value)
+        weightLabel.text = "\(weight)kg"
+    }
+}
+
