a7eae1a1450e8870145915d3c70df51db2d5aba9
Ken Mau <>
2021-12-12 18:16:22 -0500

    Lesson 152

---
+++ udemy/ios13-bootcamp/Clima-iOS13-master/Clima/Model/WeatherData.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Ken Mau on 2021-12-12.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let description: String
+    let id: Int
+}
--- udemy/ios13-bootcamp/Clima-iOS13-master/Clima/Model/WeatherManager.swift
+++ udemy/ios13-bootcamp/Clima-iOS13-master/Clima/Model/WeatherManager.swift
@@ -23,24 +23,37 @@ struct WeatherManager {
             let session = URLSession(configuration: .default)
             
             // Give Session a Task
-            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            // Trailing closure
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if let error = error {
+                    print(error)
+                    return
+                }
+                
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                }
+            }
             
             // Start the task
             task.resume()
         }
     }
-        
-    func handle(data: Data?, response: URLResponse?, error: Error?) {
-        if let error = error {
+    
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            
+            print(weather.conditionName)
+            
+        } catch {
             print(error)
-            return
         }
-        
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString)
-        }
-        
-        // Check Data
     }
 }
