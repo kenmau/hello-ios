5d9b57956ca1884a549fc2b0ae060cc2a030a12e
Ken Mau <>
2022-01-09 11:24:25 -0500

    Lesson 205 SwiftUI

---
+++ udemy/ios13-bootcamp/I Am Rich SwiftUI/I Am Rich SwiftUI/I_Am_Rich_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich_SwiftUIApp.swift
+//  I Am Rich SwiftUI
+//
+//  Created by Ken Mau on 2022-01-09.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
